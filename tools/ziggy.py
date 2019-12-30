223acd8a3fb5ed402275b8893f55ab3f3a507f84
John Mamish <john.mamish@gmail.com>
2019-12-30 08:58:42 -0600

    add unrolled zig-zag generator

---
+++ tools/ziggy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# This tool just generates an unrolled version of the jpeg zigzag sequence.
+
+# zigzag starts on [1, 0] and moves in the downwards direction
+zigdir = -1;
+zig_x = 1;
+zig_y = 0;
+
+print("X[0] = Y[0];")
+for i in range(1, 64):
+    zigidx = (zig_x + (8 * zig_y));
+    print("X[%i] = Y[%i];"%(i, zigidx))
+
+    # update zig coordinates
+    # check and see if moving in the zig direction would put us out of bounds
+    nx = zig_x + zigdir;
+    ny = zig_y - zigdir;
+    if ((nx < 0) and (ny > 7)):
+        zig_x += 1;
+        zigdir = -zigdir;
+    elif ((nx < 0) and (zigdir == -1)):
+        zig_y += 1;
+        zigdir = -zigdir;
+    elif ((ny < 0) and (zigdir == 1)):
+        zig_x += 1;
+        zigdir = -zigdir;
+    elif ((nx > 7) and (zigdir == 1)):
+        zig_y += 1;
+        zigdir = -zigdir;
+    elif ((ny > 7) and (zigdir == -1)):
+        zig_x += 1;
+        zigdir = -zigdir;
+    else:
+        zig_x = nx;
+        zig_y = ny;
