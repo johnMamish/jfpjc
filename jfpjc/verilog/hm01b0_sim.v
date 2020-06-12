a83e7fa1596379a761a482b00b55beaa4e73aacb
John Mamish <john.mamish@gmail.com>
2020-06-11 20:11:05 -0500

    add first pass of hm01b0 simulator

--- jfpjc/verilog/hm01b0_sim.v
+++ jfpjc/verilog/hm01b0_sim.v
@@ -3,33 +3,69 @@
  *
  * Its image is 320x240.
  *
- * TODO: add hsync and vsync.
- *
  * Hold nreset low to reset the
+ *
+ * the output clock will be the same as the input clock
  */
 
 `timescale 1ns/100ps
 
+`define WIDTH (320)
+`define HEIGHT (240)
+
+`define HPADDING (20)
+`define VPADDING (2)
+
 module hm01b0_sim(input      nreset,
+                  input      mclk,
+
                   output reg clock,
-                  output reg [7:0] pixdata);
+                  output reg [7:0] pixdata,
+
+                  output reg hsync,
+                  output reg vsync);
+    assign clock = mclk;
+
     parameter output_freq = 750000;
 
     reg [7:0] hm01b0_image [0:((320 * 240) - 1)];
 
-    always begin
+    reg [16:0] ptrx;
+    reg [16:0] ptry;
+
+    always @ (posedge mclk) begin
         if (nreset) begin
-            #(1000000000 / (2 * output_freq));
-            clock = ~clock;
+            if (ptrx == ((`WIDTH + `HPADDING) - 1)) begin
+                ptrx <= 16'h0;
+            end else begin
+                ptrx <= ptrx + 1;
+            end
+
+            if (ptrx == ((`WIDTH + `HPADDING) - 1)) begin
+                if (ptry == ((`HEIGHT + `VPADDING) - 1)) begin
+                    ptry <= 16'h0;
+                end else begin
+                    ptry <= ptry + 1;
+                end
+            end else begin
+                ptry <= ptry;
+            end
+
         end else begin
-            clock = 0;
-            ptr = 0;
+            ptrx <= 'h0;
+            ptry <= 'h0;
         end
     end
 
-    reg [16:0] ptr;
+    always @* begin
+        if ((ptrx < `WIDTH) && (ptry < `HEIGHT)) begin
+            pixdata <= hm01b0_image[(ptry * `WIDTH) + ptrx];
+        end else begin
+            pixdata <= 8'hxx;
+        end
 
-    always @ (posedge clock) begin
-        ptr <= ptr + 1;
+        // Unclear if this is the right vsync/hsync behavior. Need to check with a scope.
+        hsync = (ptrx < `WIDTH) ? (1'b1) : (1'b0);
+        vsync = (ptry < `HEIGHT) ? (1'b1) : (1'b0);
     end
 endmodule
