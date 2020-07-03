31bcdcc4300ee9cf0b7b62195d9d0939d30891f4
John Mamish <john.mamish@gmail.com>
2020-07-03 00:26:55 -0500

    fix bad ebr_index bug on fetch index mod 64 == 63

--- jfpjc/verilog/jfpjc.v
+++ jfpjc/verilog/jfpjc.v
@@ -256,10 +256,11 @@ module jfpjc(input                      nreset,
 `define QUANTIZER_STATE_QUANTIZE 1'h1
     reg [1:0] quantizer_readbuf;
     reg [5:0] coefficient_index [0:1];
-    reg [2:0] ebr_index;
+    reg [2:0] ebr_index [0:1];
     reg [1:0] quantizer_output_buffer [0:1];
     always @(posedge clock) begin
         if (nreset) begin
+            ebr_index[1] <= ebr_index[0];
             coefficient_index[1] <= coefficient_index[0];
             quantizer_output_buffer[1] <= quantizer_output_buffer[0];
             case (quantizer_state)
@@ -267,7 +268,7 @@ module jfpjc(input                      nreset,
                     quantizer_readbuf <= quantizer_readbuf;
                     coefficient_index[0] <= 'h0;
                     dividend_divisor_valid <= 1'h0;
-                    ebr_index <= 'h0;
+                    ebr_index[0] <= 'h0;
                     if (quantizer_readbuf != dcts_frontbuffer) begin
                         quantizer_state <= `QUANTIZER_STATE_QUANTIZE;
                     end else begin
@@ -282,19 +283,19 @@ module jfpjc(input                      nreset,
                     if (coefficient_index[0] == 'h3f) begin
                         quantizer_output_buffer[0] <= quantizer_output_buffer[0] + 'h1;
 
-                        if (ebr_index == 3'h4) begin
+                        if (ebr_index[0] == 3'h4) begin
                             quantizer_state <= `QUANTIZER_STATE_WAIT;
                             quantizer_readbuf <= quantizer_readbuf + 'h1;
-                            ebr_index <= 'h0;
+                            ebr_index[0] <= 'h0;
                         end else begin
                             quantizer_state <= `QUANTIZER_STATE_QUANTIZE;
                             quantizer_readbuf <= quantizer_readbuf;
-                            ebr_index <= ebr_index + 'h1;
+                            ebr_index[0] <= ebr_index[0] + 'h1;
                         end
                     end else begin
                         quantizer_output_buffer[0] <= quantizer_output_buffer[0];
                         quantizer_state <= `QUANTIZER_STATE_QUANTIZE;
-                        ebr_index <= ebr_index;
+                        ebr_index[0] <= ebr_index[0];
                     end
                 end
             endcase
@@ -305,7 +306,8 @@ module jfpjc(input                      nreset,
             quantizer_output_buffer[1] <= 2'h0;
             coefficient_index[0] <= 'h0;
             coefficient_index[1] <= 'h0;
-            ebr_index <= 'h0;
+            ebr_index[0] <= 'h0;
+            ebr_index[1] <= 'h0;
             dividend_divisor_valid <= 'h0;
         end
     end
@@ -317,8 +319,7 @@ module jfpjc(input                      nreset,
 
     always @* begin
         dct_output_read_addr = { quantizer_readbuf, zigzagged_coefficient_index };
-        dividend = dct_output_read_data[ebr_index];
-        //dividend = coefficient_index[1];
+        dividend = dct_output_read_data[ebr_index[1]];
     end
 
     // entries in the quantization table shall be stored in zig-zag order.
