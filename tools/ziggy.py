628f9ad0f5f3ccdc5cdc55986291a466e1d203a9
John Mamish <john.mamish@gmail.com>
2020-07-16 15:27:37 -0500

    add zigzag function in ziggy.py that's pastable in a python interpreter

--- tools/ziggy.py
+++ tools/ziggy.py
@@ -34,3 +34,35 @@ for i in range(1, 64):
     else:
         zig_x = nx;
         zig_y = ny;
+
+
+# you can copy and paste this inside a python interpreter in case you need that.
+def ziggy():
+    retval = [0]
+    for i in range(1, 64):
+        zigidx = (zig_x + (8 * zig_y));
+        retval.append(zigidx)
+        #print("X[%i] = Y[%i];"%(i, zigidx))
+        # update zig coordinates
+        # check and see if moving in the zig direction would put us out of bounds
+        nx = zig_x + zigdir;
+        ny = zig_y - zigdir;
+        if ((nx < 0) and (ny > 7)):
+            zig_x += 1;
+            zigdir = -zigdir;
+        elif ((nx < 0) and (zigdir == -1)):
+            zig_y += 1;
+            zigdir = -zigdir;
+        elif ((ny < 0) and (zigdir == 1)):
+            zig_x += 1;
+            zigdir = -zigdir;
+        elif ((nx > 7) and (zigdir == 1)):
+            zig_y += 1;
+            zigdir = -zigdir;
+        elif ((ny > 7) and (zigdir == -1)):
+            zig_x += 1;
+            zigdir = -zigdir;
+        else:
+            zig_x = nx;
+            zig_y = ny;
+    return retval
