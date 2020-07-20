johnMamish/jfpjc

commit 8b36ac08b3bf2c6fe0b9ecaca025f42a2babf321

fix bug in hm01b0 ingester that caused synthesis issues

jfpjc/verilog/hm01b0_ingester.v
@@ -40,7 +40,7 @@ module hm01b0_ingester #(parameter width_pix = 320,
         end
     endgenerate*/
 
-    reg hm01b0_pixclk_prev;
+    reg hm01b0_pixclk_prev [0:1];
     //reg [$clog2(width_pix) - 1:0] px;
     reg [2:0] px;
     reg [2:0] py;
@@ -49,11 +49,12 @@ module hm01b0_ingester #(parameter width_pix = 320,
     reg [$clog2(ebr_size / 64) - 1 : 0] mcunum_div_num_ebr;
     always @(posedge clock) begin
         if (nreset) begin
-            hm01b0_pixclk_prev <= hm01b0_pixclk;
+            hm01b0_pixclk_prev[0] <= hm01b0_pixclk;
+            hm01b0_pixclk_prev[1] <= hm01b0_pixclk_prev[0];
 
             // mind the edge direction: sparkfun code seems to think that this is rising edge, but
             // other sources disagree.
-            if ((hm01b0_pixclk && !hm01b0_pixclk_prev) &&
+            if ((!hm01b0_pixclk_prev[1] && hm01b0_pixclk_prev[0]) &&
                 (hm01b0_hsync)) begin
                 wren <= 1'b1;
                 output_pixval <= hm01b0_pixdata + 8'h80;
@@ -149,7 +150,8 @@ module hm01b0_ingester #(parameter width_pix = 320,
             output_pixval <= 'hxx;
             wren <= 'h0;
 
-            hm01b0_pixclk_prev <= 1'b0;
+            hm01b0_pixclk_prev[0] <= hm01b0_pixclk;
+            hm01b0_pixclk_prev[1] <= hm01b0_pixclk;
             px <= 'h0;
             mcunum_div_num_ebr <= 'h0;
             mcux <= 'h0;

commit 83cbd4a196f709e3f1c415db0c309dae157b852e

fix regression in hm01b0_ingester introduced by 8b36ac08b3bf2c6fe

jfpjc/verilog/hm01b0_ingester.v
@@ -10,45 +10,33 @@
  */
 `define SYNCHRONOUS
 `ifdef SYNCHRONOUS
-module hm01b0_ingester #(parameter width_pix = 320,
-                         parameter height_pix = 240,
-                         parameter num_ebr = 5,
-                         parameter ebr_size = 512)
+module hm01b0_ingester(input                      clock,
+                       input                      nreset,
 
-    (input                      clock,
-     input                      nreset,
+                       input                      hm01b0_pixclk,
+                       input [7:0]                hm01b0_pixdata,
+                       input                      hm01b0_hsync,
+                       input                      hm01b0_vsync,
 
-     input                      hm01b0_pixclk,
-     input [7:0]                hm01b0_pixdata,
-     input                      hm01b0_hsync,
-     input                      hm01b0_vsync,
+                       output reg [($clog2(num_ebr) - 1):0] output_block_select,
+                       output reg [0:0]           frontbuffer_select,
+                       output reg [($clog2(ebr_size) - 1):0] output_write_addr,
+                       output reg [7:0]           output_pixval,
+                       output reg [0:0]           wren);
+    localparam width_pix = 320, height_pix = 240, num_ebr = 5, ebr_size = 512;
 
-     output reg [($clog2(num_ebr) - 1):0] output_block_select,
-     output reg [0:0]           frontbuffer_select,
-     output reg [($clog2(ebr_size) - 1):0] output_write_addr,
-     output reg [7:0]           output_pixval,
-     output reg [0:0]           wren);
 
-    // we need enough EBRs to hold all the pixels
-    /*generate
-        if (((width_pix % 8) != 0) ||
-            ((height_pix % 8) != 0) ||
-            (width_pix * 8) >= (num_ebr * ebr_size)) begin
-        if (0) begin
-            __error_module invalid_parameters();
-        end else begin
-        end
-    endgenerate*/
-
+    reg [7:0] hm01b0_pixdata_prev;
     reg hm01b0_pixclk_prev [0:1];
-    //reg [$clog2(width_pix) - 1:0] px;
     reg [2:0] px;
     reg [2:0] py;
     reg [$clog2(width_pix / 8) - 1 : 0] mcux;
+
     // TODO: does this name make sense given the consts used to size it?
     reg [$clog2(ebr_size / 64) - 1 : 0] mcunum_div_num_ebr;
     always @(posedge clock) begin
         if (nreset) begin
+            hm01b0_pixdata_prev <= hm01b0_pixdata;
             hm01b0_pixclk_prev[0] <= hm01b0_pixclk;
             hm01b0_pixclk_prev[1] <= hm01b0_pixclk_prev[0];
 
@@ -57,7 +45,7 @@ module hm01b0_ingester #(parameter width_pix = 320,
             if ((!hm01b0_pixclk_prev[1] && hm01b0_pixclk_prev[0]) &&
                 (hm01b0_hsync)) begin
                 wren <= 1'b1;
-                output_pixval <= hm01b0_pixdata + 8'h80;
+                output_pixval <= hm01b0_pixdata_prev + 8'h80;
             end else begin
                 wren <= 1'b0;
                 output_pixval <= 8'hxx;
@@ -146,7 +134,6 @@ module hm01b0_ingester #(parameter width_pix = 320,
             // RESET
             output_block_select <= 'h0;
             frontbuffer_select <= 'h0;
-            //output_write_addr <= 'h0;
             output_pixval <= 'hxx;
             wren <= 'h0;
 

jfpjc/verilog/hm01b0_sim.v
@@ -23,7 +23,7 @@ module hm01b0_sim(input      mclk,
                   input      nreset,
 
                   output reg clock,
-                  output wire [7:0] pixdata,
+                  output reg  [7:0] pixdata,
 
                   output reg hsync,
                   output reg vsync);
@@ -33,7 +33,7 @@ module hm01b0_sim(input      mclk,
     reg [15:0] ptrx;
     reg [15:0] ptry;
 
-    always @ (posedge mclk) begin
+    always @ (negedge mclk) begin
         if (nreset) begin
             if (ptrx == ((`WIDTH + `HPADDING) - 1)) begin
                 ptrx <= 16'h0;
@@ -59,9 +59,14 @@ module hm01b0_sim(input      mclk,
 
     // for some reason, putting hm01b0_image (an array of 76800 items) inside an always @* block
     // really upsets iverilog, so we need to use these assign statements on pixdata.
-    assign pixdata = ((ptrx < `WIDTH) && (ptry < `HEIGHT)) ?
-                     hm01b0_image[(ptry * `WIDTH) + ptrx] :
-                     8'hxx;
+    wire [7:0] pixdata_i;
+    assign pixdata_i = ((ptrx < `WIDTH) && (ptry < `HEIGHT)) ?
+                       hm01b0_image[(ptry * `WIDTH) + ptrx] :
+                       8'hxx;
+    always @* begin
+        // in case you want to add a delay
+        pixdata = pixdata_i;
+    end
 
     always @* begin
         // Unclear if this is the right vsync/hsync behavior. Need to check with a scope.