acec9abde04105fd073ace2fd1e1d8363b811f58
John Mamish <john.mamish@gmail.com>
2020-07-20 20:53:18 -0500

    add image error tool for testing

---
+++ tools/image_error.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# This tool finds the magnitude of the difference between 2 images of the same dimensions.
+import numpy as np
+from PIL import Image
+import PIL
+import sys
+if __name__ == "__main__":
+    if (len(sys.argv) != 3):
+        print("2 args are required. Usage: " + sys.argv[0] + " <image 1> <image 2>\n");
+        quit(0)
+
+    try:
+        img1 = Image.open(sys.argv[1])
+        img2 = Image.open(sys.argv[2])
+    except (FileNotFoundError) as e:
+        print(e)
+        quit(0)
+
+    print(np.linalg.norm(np.array(img1).flatten() - np.array(img2).flatten()))
