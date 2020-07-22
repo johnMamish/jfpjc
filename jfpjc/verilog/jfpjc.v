d9d81e0ccddc27594e1553095b6df309f87d0b1c
John Mamish <john.mamish@gmail.com>
2020-07-21 21:39:11 -0500

    move dct reset logic to seperate file

--- jfpjc/verilog/jfpjc.v
+++ jfpjc/verilog/jfpjc.v
@@ -10,9 +10,6 @@
  * After this, a configurable number of DCT engines read from each of the EBRs in sequence
  */
 
-
-//`include "hm01b0_ingester.v"
-
 `timescale 1ns/100ps
 
 module jfpjc(input                      nreset,
@@ -95,12 +92,11 @@ module jfpjc(input                      nreset,
     // Note that the 5 EBRs here make a special case where I can just have 1 DCT engine per EBR. In
     // other cases, I'd need a routing network between the EBRs and the DCT engines to correctly
     // distribute N EBRs worth of data over M DCT engines.
-    reg [1:0] dcts_frontbuffer;
-    reg       dct_nreset;
+    wire [1:0] dcts_frontbuffer;
+    wire       dct_nreset;
     wire [5:0] dct_fetch_addr [0:4];
     wire [4:0] dcts_finished;
-    reg [2:0] mcu_groups_processed;
-
+    wire [2:0] mcu_groups_processed;
     reg [7:0] dct_output_read_addr;
     wire signed [15:0] dct_output_read_data [0:4];
 
@@ -152,115 +148,13 @@ module jfpjc(input                      nreset,
 
     ////////////////////////////////////////////////////////////////
     // DCT engine reset logic
-    // This logic should be in a seperate module. It watches the ingester unit and resets the
-    // DCT engines when there is a new buffer that can be processed.
-    //
-    // When the backbuffer changes, this state machine holds the DCT engines in reset for 3 clock
-    // cycles, then sets them loose with new base addresses and
-`define DCTS_STATE_WAIT_FRAMEBUFFER 3'h0
-`define DCTS_STATE_RESET 3'h1
-`define DCTS_STATE_DCTS_ACTIVE 3'h2
-`define DCTS_STATE_ERR 3'h3
-    reg [2:0] DCTs_state;
-    reg [1:0] reset_cnt;
-    reg       ingester_frontbuffer [0:1];
-    wire      buffer_swapped;
-    assign buffer_swapped = (ingester_frontbuffer[0] != ingester_frontbuffer[1]);
-    always @ (posedge clock) begin
-        if (nreset) begin
-            ingester_frontbuffer[0] <= ingester_frontbuffer_select;
-            ingester_frontbuffer[1] <= ingester_frontbuffer[0];
-
-            case (DCTs_state)
-                `DCTS_STATE_WAIT_FRAMEBUFFER: begin
-                    mcu_groups_processed <= 'h0;
-                    reset_cnt <= 'h0;
-                    dcts_frontbuffer <= dcts_frontbuffer;
-                    if (buffer_swapped) begin
-                        DCTs_state <= `DCTS_STATE_RESET;
-                    end else begin
-                        DCTs_state <= DCTs_state;
-                    end
-                end
-
-                `DCTS_STATE_RESET: begin
-                    mcu_groups_processed <= mcu_groups_processed;
-                    dcts_frontbuffer <= dcts_frontbuffer;
-                    if (buffer_swapped) begin
-                        reset_cnt <= 'h0;
-                        DCTs_state <= `DCTS_STATE_ERR;
-                    end else if (reset_cnt < 'h2) begin
-                        reset_cnt <= reset_cnt + 'h1;
-                        DCTs_state <= `DCTS_STATE_RESET;
-                    end else begin
-                        reset_cnt <= 'h0;
-                        DCTs_state <= `DCTS_STATE_DCTS_ACTIVE;
-                    end
-                end
-
-                `DCTS_STATE_DCTS_ACTIVE: begin
-                    reset_cnt <= 'h0;
-
-                    if (buffer_swapped) begin
-                        mcu_groups_processed <= mcu_groups_processed;
-                        DCTs_state <= `DCTS_STATE_ERR;
-                        dcts_frontbuffer <= dcts_frontbuffer;
-                    end else if (&dcts_finished) begin
-                        mcu_groups_processed <= mcu_groups_processed + 'h1;
-                        if (mcu_groups_processed == 'h7) begin
-                            DCTs_state <= `DCTS_STATE_WAIT_FRAMEBUFFER;
-                            dcts_frontbuffer <= dcts_frontbuffer + 'h1;
-                        end else begin
-                            DCTs_state <= `DCTS_STATE_RESET;
-                            dcts_frontbuffer <= dcts_frontbuffer + 'h1;
-                        end
-                    end else begin
-                        mcu_groups_processed <= mcu_groups_processed;
-                        DCTs_state <= `DCTS_STATE_DCTS_ACTIVE;
-                        dcts_frontbuffer <= dcts_frontbuffer;
-                    end
-                end
-
-                `DCTS_STATE_ERR: begin
-                    mcu_groups_processed <= 'hx;
-                    reset_cnt <= 'hx;
-                    DCTs_state <= `DCTS_STATE_ERR;
-                    dcts_frontbuffer <= 'hx;
-                end
-            endcase
-        end else begin
-            DCTs_state <= `DCTS_STATE_WAIT_FRAMEBUFFER;
-            ingester_frontbuffer[0] <= 1'b0;
-            ingester_frontbuffer[1] <= 1'b0;
-            mcu_groups_processed <= 'h0;
-            reset_cnt <= 'h0;
-            dcts_frontbuffer <= 'h0;
-        end
-    end
-
-    always @* begin
-        case (DCTs_state)
-            `DCTS_STATE_WAIT_FRAMEBUFFER: begin
-                dct_nreset = 1'b0;
-            end
-
-            `DCTS_STATE_RESET: begin
-                dct_nreset = 1'b0;
-            end
-
-            `DCTS_STATE_DCTS_ACTIVE: begin
-                dct_nreset = 1'b1;
-            end
-
-            `DCTS_STATE_ERR: begin
-                dct_nreset = 1'b1;
-            end
-
-            default: begin
-                dct_nreset = 1'bx;
-            end
-        endcase // case (DCTs_state)
-    end
+    dct_manager_fsm dct_manager(.clock(clock),
+                                .nreset(nreset),
+                                .ingester_frontbuffer_select(ingester_frontbuffer_select),
+                                .dcts_finished(&dcts_finished),
+                                .mcu_groups_processed(mcu_groups_processed),
+                                .dcts_frontbuffer(dcts_frontbuffer),
+                                .dct_nreset(dct_nreset));
 
     ////////////////////////////////////////////////////////////////
     // quantizer
